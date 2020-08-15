d90c0b8c91a123336da93f952d6796e3f617d3fc
JACK-QBS <2200838299@qq.com>
2020-08-15 14:27:21 +0800

    上传代码

---
+++ first.c
@@ -0,0 +1,95 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include<stdio.h>
+
+int main()
+{
+	int day = 0;
+	switch (day)
+	{
+	case 1:
+		printf("星期一\n");
+		break;
+	case 2:
+		printf("星期二\n");
+		break;
+	case 3:
+		printf("星期三\n");
+		break;
+	case 4:
+		printf("星期四\n");
+		break;
+	case 5:
+		printf("星期五\n");
+		break;
+	case 6:
+		printf("星期六\n");
+		break;
+	case 7:
+		printf("星期天\n");
+		break;
+	}
+	return 0;
+}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 2;
+//	if (a == 1)
+//		if (b == 2)
+//			printf("haha\n");
+//		else
+//			printf("hehe\n");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	if (age < 18)
+//	{
+//		printf("青少年\n");
+//	}
+//	else if (age >= 18 && age < 30)
+//	{
+//		printf("年轻人");
+//	}
+//	else if (age >= 30 && age < 60)
+//	{
+//		printf("中年人\n");
+//	}
+//	else
+//	{
+//		printf("老年人\n");
+//	}
+//}
+
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	if (age < 18)
+//	{
+//		printf("未成年\n");
+//	}
+//	else
+//	{
+//		printf("成年\n");
+//	}
+//}
+
+
+//
+////代码1
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	if (age, 18)
+//	{
+//		printf("未成年\n");
+//	}
+//}
