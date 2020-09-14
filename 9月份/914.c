15ef2b7ec5e22864ac5f978a4cecbea2423a46e9
JACK-QBS <2200838299@qq.com>
2020-09-14 22:38:50 +0800

    操作符题

---
+++ 9月份/914.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WAARNINGS 1
+#include<stdio.h>
+
+void Printbit(int num)
+{
+	for (int i = 31; i >= 1; i -= 2)
+	{
+		printf("%d ", (num >> i) & 1);
+	}
+	printf("\n");
+	for (int i = 30; i >= 0; i -= 2)
+	{
+		printf("%d ", (num >> i) & 1);
+	}
+	printf("\n");
+}
+int main()
+{
+	int num = 0;
+	printf("请输入一个数： ");
+	scanf("%d", &num);
+	Printbit(num);
+	return 0;
+}
+
+//int main()
+//{
+//	int a, b, c;
+//	a = 5;
+//	c = ++a;
+//	b = ++c, c++, ++a, a++;
+//	b += a++ + c;
+//	printf("a=%d b=%d c=%d \n", a, b, c);
+//	return 0;
+//}
+
+
+//int i;
+//int main()
+//{
+//	i--;
+//	if (i > sizeof(i))
+//	{
+//		printf(">\n");
+//	}
+//	else
+//	{
+//		printf("<\n");
+//	}
+//	return 0;
+//}
+
+
